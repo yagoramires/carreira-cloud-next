c77d28e35e2fc382c82938ed961f50f8326b3b57
D2B <suporte@d2bonline.com.br>
2022-07-13 14:31:50 -0300

    newsletter component init, footer css fix

---
+++ src/components/Footer/Newsletter/Newsletter.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from 'react';
+
+import styles from './newsletter.module.scss';
+
+export default function Footer() {
+  return (
+    <div className="div"></div>
+  )
+}
